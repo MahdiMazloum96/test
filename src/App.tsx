fc741e85769ba1dfe6a1bdca3e063ca76f616146
uchihaghost <m.mazloum75@gmail.com>
2024-12-13 23:53:37 +0330

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <div className="text-7xl">hello</div>
+      <div className="text-3xl text-red-900 ">App</div>
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/index.html", // Include index.html
+    "./src/**/*.{js,ts,jsx,tsx}", // Include all source files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
