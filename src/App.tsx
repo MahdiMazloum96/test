4ab60b041ced4f647d345048f57b61e953b729c7
uchihaghost <m.mazloum75@gmail.com>
2024-12-14 00:24:24 +0330

    tailwind configs

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ const App = () => {
   return (
     <div>
       <div className="text-7xl">hello</div>
-      <div className="text-3xl text-red-900 ">App</div>
+      <div className="text-3xl text-red-900  bg-red-700">App</div>
     </div>
   );
 };
